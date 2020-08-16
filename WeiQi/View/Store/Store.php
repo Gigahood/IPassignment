1065be6100c2c00d4168cd82b0ebaf7a02f8bbd8
joeyelim <43492692+joeyelim@users.noreply.github.com>
2020-08-16 23:47:03 +0800

    added input validation

--- WeiQi/View/Store/Store.php
+++ WeiQi/View/Store/Store.php
@@ -1,12 +1,16 @@
 <?php
- include('../MasterPage.html');
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>this is Store Page</h1>
-    </body>
-</html>
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Store
+ *
+ * @author Lim Yi En
+ */
+class Store {
+    
+}
