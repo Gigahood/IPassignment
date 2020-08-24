57fe2aa4b3f7d61054ad1019926069a87437e6a6
Kuek Yong Boon <kuekyb@gmail.com>
2020-08-24 12:57:31 +0800

    Finish Create XML

--- WeiQi/View/HistoryModule/HistoryView.php
+++ WeiQi/View/HistoryModule/HistoryView.php
@@ -1,6 +1,7 @@
 <?php
 include('../MasterPage.html');
 require '../../Controller/History/HistoryController.php';
+require '../../Controller/History/HistoryXMLController.php';
 
 session_start();
 ?>
@@ -25,8 +26,6 @@ session_start();
                 $_SESSION["role"] = "admin";
                 $compID = $_SESSION["compID"];
                 $compHistory = getHistoryDetail($compID, $_SESSION["role"]);
-                
-
 
                 echo "<h1> " . $compHistory->getName() . " Competition History</h1>";
                 echo "<h3> Date : " . date('d/m/Y', strtotime($compHistory->getHstart())) . " - " . date('d/m/Y', strtotime($compHistory->getHend())) . "</h3>"
@@ -37,6 +36,17 @@ session_start();
                     echo "<button onCLick=\"window.location='EditHistory.php?id=" . $_SESSION["compID"] . "'\">" . "Edit History</button>";
                 }
                 ?>
+                
+                <form method="POST">
+                    <input type = "Submit" name = "xml" value = "PrintXML" />
+                </form>
+                
+                <?php
+                if(isset($_POST['xml'])) {
+                    generateXML($compID, $_SESSION["role"]);
+                }
+                ?>
+                
                 <div>
                     <?php
                     echo "<div style='text-align:center;'> Description : " . $compHistory->getRemark() . "</div>";
@@ -55,13 +65,13 @@ session_start();
                         <?php
                         foreach ($compHistory->getMatches() as $value) {
                             echo "<tr>";
-                            echo "<td style='text-align:center;'>" . getName($value["black_User"],$_SESSION["role"]) . " vs " . getName($value["white_User"],$_SESSION["role"]) . "</td>";
-                            echo "<td>" . getName($value["black_User"],$_SESSION["role"]). "</td>";
-                            echo "<td>" . getName($value["white_User"],$_SESSION["role"]) . "</td>";
+                            echo "<td style='text-align:center;'>" . getName($value["black_User"], $_SESSION["role"]) . " vs " . getName($value["white_User"], $_SESSION["role"]) . "</td>";
+                            echo "<td>" . getName($value["black_User"], $_SESSION["role"]) . "</td>";
+                            echo "<td>" . getName($value["white_User"], $_SESSION["role"]) . "</td>";
 
                             if ($_SESSION["role"] == "admin") {
                                 echo "<td data-id='" . $value["match_ID"] . "'>"
-                                . "<a style='margin-left: 5px;' class='detailLink' href='' data-link='". $value["match_ID"] ."' data-href='ViewMatch.php' >Board</a>"
+                                . "<a style='margin-left: 5px;' class='detailLink' href='' data-link='" . $value["match_ID"] . "' data-href='ViewMatch.php' >Board</a>"
                                 . "<a style='margin-left: 5px;' class='link' href='EditMatch.php' data-href='EditMatch.php'>Edit</a>"
                                 . "<a style='margin-left: 5px;' class='link' href='HistoryDetail.php' data-href='ViewMatch.php'>Delete</a>"
                                 . "</td>";
@@ -90,11 +100,11 @@ session_start();
                     foreach ($listOfMatches as $match) {
                         echo "<tr>";
                         echo "<td style='text-align:center;'>" . $index . "</td>";
-                        echo "<td style='text-align:center;'>" . getName($match["Name"],$_SESSION["role"]) . "</td>";
+                        echo "<td style='text-align:center;'>" . getName($match["Name"], $_SESSION["role"]) . "</td>";
                         echo "<td style='text-align:center;'>" . $match["Score"] . "</td>";
                         $index += 1;
                     }
-                    
+
                     closeCon($_SESSION["role"]);
                     ?>
                 </table>
@@ -113,7 +123,7 @@ session_start();
                             + this.parentNode.getAttribute('data-id') + "";
                 });
             }
-            
+
             var b = document.getElementsByClassName("detailLink");
 
             for (let i = 0; i < b.length; i++) {
--- WeiQi/View/HistoryModule/Test/printXML.php
+++ WeiQi/View/HistoryModule/Test/printXML.php
@@ -1,25 +1,48 @@
 <?php
 
 /* create a dom document with encoding utf8 */
+//$domtree = new DOMDocument('1.0', 'UTF-8');
+//
+///* create the root element of the xml tree */
+//$xmlRoot = $domtree->createElement("xml");
+///* append it to the document created */
+//$xmlRoot = $domtree->appendChild($xmlRoot);
+//
+//$currentTrack = $domtree->createElement("track");
+//$currentTrack = $xmlRoot->appendChild($currentTrack);
+//
+///* you should enclose the following two lines in a cicle */
+//$currentTrack->appendChild($domtree->createElement('path', 'song1.mp3'));
+//$currentTrack->appendChild($domtree->createElement('title', 'title of song1.mp3'));
+//
+//$currentTrack->appendChild($domtree->createElement('path', 'song2.mp3'));
+//$currentTrack->appendChild($domtree->createElement('title', 'title of song2.mp3'));
+
 $domtree = new DOMDocument('1.0', 'UTF-8');
 
 /* create the root element of the xml tree */
-$xmlRoot = $domtree->createElement("xml");
+$xmlRoot = $domtree->createElement("competitions");
 /* append it to the document created */
 $xmlRoot = $domtree->appendChild($xmlRoot);
 
-$currentTrack = $domtree->createElement("track");
-$currentTrack = $xmlRoot->appendChild($currentTrack);
+$currentCompetition = $domtree->createElement("competition");
+$currentCompetition = $xmlRoot->appendChild($currentCompetition);
 
-/* you should enclose the following two lines in a cicle */
-$currentTrack->appendChild($domtree->createElement('path', 'song1.mp3'));
-$currentTrack->appendChild($domtree->createElement('title', 'title of song1.mp3'));
+$currentCompetition->appendChild($domtree->createElement('competitionID', '1'));
+$currentCompetition->appendChild($domtree->createElement('competitionName', 'WeiQi May Competition PJ'));
 
-$currentTrack->appendChild($domtree->createElement('path', 'song2.mp3'));
-$currentTrack->appendChild($domtree->createElement('title', 'title of song2.mp3'));
+$history = $domtree->createElement("history");
+$history = $currentCompetition->appendChild($history);
+
+$history->appendChild($domtree->createElement("historyStartDate", "123"));
+
+//$currentCompetition->appendChild($domtree->createElement("history"));
+//$currentCompetition->appendChild($domtree->createElement("historyStartDate", "123"));
+
+
 
 /* get the xml printed */
 echo $domtree->saveXML();
 
-$domtree->save('test1.xml');
+$domtree->save('test3.xml');
 ?>
