e00c1f90f110d56ae946e0842c27c629a61531dc
Kuek Yong Boon <kuekyb@gmail.com>
2020-08-24 23:18:35 +0800

    update

---
+++ WeiQi/View/Store/client.php
@@ -0,0 +1,48 @@
+<h2>Get Product</h2>
+<form method="POST">
+    <p>
+        Type In Product Name :  <input type="text" name="name" value="" /> </br></br></br>
+    </p>
+
+    <input type="submit" value="Submit" name="submit" />
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+    if (empty($_POST["name"])) {
+        echo "<h3>Please enter Product Name</h3>";
+    } else {
+        $name = $_POST["name"];
+    }
+
+    if (!empty($name)) {
+        $url = "http://localhost/IPassignment/WeiQi/View/Store/service.php?name=" . urlencode($name);
+
+        $client = curl_init($url);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($client);
+        $result = json_decode($response, true);
+
+        function display($array) {
+            echo "<p>Product Name : " . $array["pro_name"] . "</p>";
+            echo "<p>Product Description : " . $array["pro_desc"] . "</p>";
+            echo "<p>Product Price  : " . $array["normal_price"] . "</p>";
+            echo "<p>Product Member Price : " . $array["discount_rate"] . "</p>";
+        }
+
+        if (empty($result)) {
+            echo "No Products Found";
+        } else {
+            echo "<h1>Product Detail</h1>";
+
+            foreach ($result['product'] as $object) {
+                echo "<h3>Product</h3>";
+                display($object);
+                echo "<br/>";
+                echo "<br/>";
+                echo "<br/>";
+            }
+        }
+    }
+}
+?>
