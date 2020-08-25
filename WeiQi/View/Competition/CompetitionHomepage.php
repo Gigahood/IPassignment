1df5479f0912d60467fc79063dc5936083e0a67f
Kuek Yong Boon <kuekyb@gmail.com>
2020-08-25 11:10:35 +0800

    done ajax

--- WeiQi/View/Competition/CompetitionHomepage.php
+++ WeiQi/View/Competition/CompetitionHomepage.php
@@ -10,8 +10,8 @@ and open the template in the editor.
  * @author Cheng Qing Xiang
  *-->
 <?php
-    include '../MasterPage.php';
-    include_once '../../Model/CompetitionDBConnection.php';
+include '../MasterPage.php';
+include_once '../../Model/CompetitionDBConnection.php';
 ?>
 <html>
     <head>
@@ -23,54 +23,105 @@ and open the template in the editor.
         <div style="text-align: center;">
             <h1>Search Competition</h1>
             <br /><br /><br />
-            
-            <div style="font-size: 20px;">
+
+            <!--            <button  onclick="ajaxFunction()">BUTTON</button>-->
+
+            <div>
+                <?php
+                $db = CompetitionDBConnection::getInstance("admin");
+                $result = $db->retrieveCompetition();
+
+                $str = " <select id='ddID' onchange=\"getData()\">";
+
+                foreach ($result as $row) {
+                    $str .= "<option value='" . $row["competition_name"] . "'>" . $row["competition_name"] . "</option>";
+                }
+
+                $str .= "</select>";
+
+                echo $str;
+                ?>
+
+
+            </div>
+
+            <div id="printID"  style="font-size: 20px;">
                 Competition Name : 
-                
+
                 <!--Options need to bind from DB by concatenate(combine)competition name & year -->
                 <!--<select name="ddlCompetitionName">
                     <option value="01">WeiQi Middle Class Match (2016)</option>
                     <option value="02">WeiQi Student Grade 2 Exam (2017)</option>
                 </select>-->
-                
+
                 <?php
-                session_start();
+//                session_start();
                 $_SESSION["role"] = "admin";
-                
-                $compName = "";
-                
-                $db = CompetitionDBConnection::getInstance($_SESSION["role"]);
-                $result = $db->retrieveCompetition();
-                print_r($result);
-                //$str = "<table> ";
-                        
-                //echo $result;
-                //echo $str;
-                $db = null;
+
+                echo "<input type='hidden' id='role' value='". $_SESSION["role"] ."'/>"
                 ?>
             </div>
             <br /><br /><br /><br /><br />
-            
+
             <div>
                 <a href="NewCompetition.php">
                     <input type="submit" value="ADD NEW COMPETITION" name="btnAddComp" class="btnStyle" style="background-color: yellow;"/>
                 </a>
                 &emsp;&emsp;&emsp;
-                <input type="submit" value="COMPETITION DETAIL" name="btnCompDet" class="btnStyle" style="background-color: red;"/>
+                <input id="btnSub" type="submit" value="COMPETITION DETAIL" name="btnCompDet" class="btnStyle" style="background-color: red;"/>
             </div>
         </div>
 
-        <?php
-        // put your code here
-        
-        
-        
-        
-        
-        
-        
-         
-        
-        ?>
+        <script language = "javascript" type = "text/javascript">
+            //Browser Support Code
+            
+            function getData() {
+              ajaxFunction();
+            }
+            
+            function ajaxFunction() {
+                var ajaxRequest;  // The variable that makes Ajax possible!
+ 
+
+                try {
+                    // Opera 8.0+, Firefox, Safari
+                    ajaxRequest = new XMLHttpRequest();
+                } catch (e) {
+
+                }
+
+                // Create a function that will receive data 
+                // sent from the server and will update
+                // div section in the same page.
+
+                ajaxRequest.onreadystatechange = function () {
+                    if (ajaxRequest.readyState === 4) {
+                        var ajaxDisplay = document.getElementById('printID');
+                        ajaxDisplay.innerHTML = ajaxRequest.responseText;
+                    }
+                };
+
+
+
+                var gender = document.getElementById('ddID').value;
+
+                var params = "name=" + gender;
+
+                ajaxRequest.open("POST", "AJAX.php", true);
+                ajaxRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                ajaxRequest.send(params);
+            }
+            
+            document.getElementById("btnSub").addEventListener("click", function(e){
+                let valid = document.getElementById("valid").value;
+                let id = document.getElementById("compID").getAttribute('data-id');;
+                
+                if (valid === "true"){
+                    window.location.href = "../HistoryModule/HistoryView.php?from=comp&id=" + id;
+                }else {
+                    window.location.href = "../HistoryModule/CreateHistoryAdmin.php?from=comp&id=" + id;
+                }
+            })
+        </script>
     </body>
 </html>
